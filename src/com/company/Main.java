824d95a91da990ea2d302fab3f34393e66e4205a
Maria Wiik <wiikcandido.maria@syk.fi>
2022-08-29 18:52:38 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int a = 12, b = 32;
+        if ((a-b)%10 == 0)
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+}
